4819ebb66e211369ccaab71b0c684104aa3c0d2e
Charles Givre <cgivre@apache.org>
2021-11-10 21:19:51 -0500

    Updated Docs

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # DataDistillr Python SDK
-This library allows you to programmatically interact with DataDistillr.
+
+[![Total alerts](https://img.shields.io/lgtm/alerts/g/datadistillr/datadistillr-python-sdk.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/datadistillr/datadistillr-python-sdk/alerts/)
+[![Language grade: Python](https://img.shields.io/lgtm/grade/python/g/datadistillr/datadistillr-python-sdk.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/datadistillr/datadistillr-python-sdk/context:python)
+
+This library allows you to programmatically interact with DataDistillr.  It is quite simple to programmatically pull data 
+from DataDistillr for use in machine learning. 
+
+## Methods
+* `get_dataframe(url, auth_token)`: Pulls your data and returns it in a Pandas DataFrame.
+* `get_csv_from_api(url, auth_token, filename)`:  Pulls your data and returns it in a CSV file.
+* `get_json_from_api(url, auth_token, filename)`:  Pulls your data and returns it in a JSON file.
+* `get_parquet_from_api(url, auth_token, filename)`:  Pulls your data and returns it in a parquet file.
+* `get_excel_from_api(url, auth_token, filename)`:  Pulls your data and returns it in an Excel file.
+* `get_dict_from_api(url, auth_token, filename)`:  Pulls your data and returns it in a Python dictionary.
+
+### Getting your Endpoint URL and Authorization Token
+See https://docs.datadistillr.com/ddr/ for complete documentation on obtaining the URL and Auth Token.
+
+### Usage 
+Using the SDK in Python code is quite simple.  See the snippet below:
+```python
+import datadistillr.datadistillr as ddr
+url = <Your URL From DataDistillr>
+auth_token = <AUTH TOKEN>
+dataframe = ddr.datadistillr.get_dataframe(url, auth_token)
+```
--- datadistillr/datadistillr.py
+++ datadistillr/datadistillr.py
@@ -1,13 +1,14 @@
 import pandas as pd
 import requests
 from datadistillr import AuthorizationException
-
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 
 class datadistillr:
 
     @staticmethod
     def get_dataframe(url, api_key):
         '''
+        This function allows you to programmatically access data from DataDistillr and push it to a pandas DataFrame.
         DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
         need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
         API Endpoints section.
@@ -20,7 +21,11 @@ class datadistillr:
         :return: A Pandas DataFrame of your data.
         '''
         headers = {"Authorization": api_key}
-        response = requests.get(url, headers=headers, verify=False)
+        if "devapp.datadistillr" in url:
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+            response = requests.get(url, headers=headers, verify=False)
+        else:
+            response = requests.get(url, headers=headers)
 
         # Case for unauthorized access
         if response.status_code == 401 or response.status_code == 403:
@@ -29,3 +34,97 @@ class datadistillr:
 
         return pd.DataFrame(response.json()['results'], columns=response.json()['summary']['columnNames'])
 
+    @staticmethod
+    def get_csv_from_api(url, api_key, filename):
+        '''
+        This function allows you to programmatically access data from DataDistillr and push it to a CSV file.
+        DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
+        need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
+        API Endpoints section.
+
+        If the authorization is not successful this function throws an AuthorizationException.
+
+        Full documentation is available here: https://docs.datadistillr.com/ddr/
+        :param url:  Your dataset API URL
+        :param api_key: Your unique dataset API key
+        :param filename: The filename where you want your data written
+        :return: A CSV file of your data.
+        '''
+        df = datadistillr.get_dataframe(url, api_key)
+        return df.to_csv(filename)
+
+    @staticmethod
+    def get_json_from_api(url, api_key, filename):
+        '''
+        This function allows you to programmatically access data from DataDistillr and push it to a JSON file.
+        DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
+        need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
+        API Endpoints section.
+
+        If the authorization is not successful this function throws an AuthorizationException.
+
+        Full documentation is available here: https://docs.datadistillr.com/ddr/
+        :param url:  Your dataset API URL
+        :param api_key: Your unique dataset API key
+        :param filename: The filename where you want your data written
+        :return: A JSON file of your data.
+        '''
+        df = datadistillr.get_dataframe(url, api_key)
+        return df.to_json(filename)
+
+    @staticmethod
+    def get_parquet_from_api(url, api_key, filename):
+        '''
+        This function allows you to programmatically access data from DataDistillr and push it to a parquet file.
+        DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
+        need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
+        API Endpoints section.
+
+        If the authorization is not successful this function throws an AuthorizationException.
+
+        Full documentation is available here: https://docs.datadistillr.com/ddr/
+        :param url:  Your dataset API URL
+        :param api_key: Your unique dataset API key
+        :param filename: The filename where you want your data written
+        :return: A parquet file of your data.
+        '''
+        df = datadistillr.get_dataframe(url, api_key)
+        return df.to_parquet(filename)
+
+    @staticmethod
+    def get_excel_from_api(url, api_key, filename):
+        '''
+        This function allows you to programmatically access data from DataDistillr and push it to an Excel file.
+        DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
+        need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
+        API Endpoints section.
+
+        If the authorization is not successful this function throws an AuthorizationException.
+
+        Full documentation is available here: https://docs.datadistillr.com/ddr/
+        :param url:  Your dataset API URL
+        :param api_key: Your unique dataset API key
+        :param filename: The filename where you want your data written
+        :return: An Excel file of your data.
+        '''
+        df = datadistillr.get_dataframe(url, api_key)
+        return df.to_excel(filename)
+
+    @staticmethod
+    def get_dict_from_api(url, api_key, filename):
+        '''
+        This function allows you to programmatically access data from DataDistillr and push it to a Python dictionary.
+        DataDistillr allows you to publish your data by generating an API Endpoint.  To access your data, you will
+        need an endpoint URL and an Authorization token. You can obtain both of these items in DataDistillr under the
+        API Endpoints section.
+
+        If the authorization is not successful this function throws an AuthorizationException.
+
+        Full documentation is available here: https://docs.datadistillr.com/ddr/
+        :param url:  Your dataset API URL
+        :param api_key: Your unique dataset API key
+        :param filename: The filename where you want your data written
+        :return: A Python dictionary file of your data.
+        '''
+        df = datadistillr.get_dataframe(url, api_key)
+        return df.to_dict(filename)
