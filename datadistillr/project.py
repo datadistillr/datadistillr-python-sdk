6edf5ccdea33450248a4fa34d8bcbe405139b5b3
Amandaha29-ddr <amanda.ha@datadistillr.com>
2022-03-25 10:22:25 -0700

    added get projects, get orgs, and login

---
+++ datadistillr/project.py
@@ -0,0 +1,10 @@
+class project:
+
+    def __init__(self, proj_details, curr_session):
+        # stores the cookies so you can make requests (pass around cookie)
+        self.session = curr_session
+        self.details_json = proj_details
+        self.name = self.details_json["name"]
+        self.project_token = self.details_json["token"]
+
+        pass
